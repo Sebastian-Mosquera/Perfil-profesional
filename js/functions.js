1261755f3b3fb395d038429dacd5f8f44fcc933b
Sebastian-Mosquera <jsmosquerab@ufpso.edu.co>
2023-11-03 20:20:06 +0100

    a

--- js/functions.js
+++ js/functions.js
@@ -45,7 +45,7 @@ var boolean = true; //false
 
 // arrays - vectores 
 var array_num = [1,2,3,45,3.4,1.2];
-var array_tex = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
+var array_text = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 var array_mix = [1, "a",2,"c"];
 var array_mul = [
     {name:"Sebastián", last_name:"Mosquera",age:"20"},
@@ -72,3 +72,80 @@ console.log("División"  + divi);
 // modulo
 var modulo = number_one % number_two;
 console.log("Módulo"  + modulo);
+
+
+
+// OPERADORES LÓGICOS Y ESTRUCTURAS CONDICIONALES (INVERSE, INCREMNETO Y DEMCREMENTO)
+// AND && con if - Ampersand
+// var bool = false;
+// var numeric =10;
+// if(!bool && numeric == "10"){ // bool == true
+//     console.log("Entra if");
+// }
+// else{
+//     console.log("Entra else");
+// }
+// // OR || con if 
+// if(!bool || numeric == 7 ){
+//     console.log("Entra if");
+//     numeric+=3;
+// }else{
+//     console.log("Entra else");
+//     numeric--;
+// }
+
+// // FOR
+// for(let j = 0; j<array_text.length; j++){
+//     console.log(array_text[j] + (j+1));
+// }
+
+// // WHILE
+// let j = 0;
+// while(j<array_text.length){
+//     console.log(array_text[j] + (j+1));
+// }
+
+// // DO WHILE
+// let m = 0;
+// do{
+//     console.log(array_text[m] + (m+1));
+//     m++;
+// }while(m<array_text.length);
+
+//FUNCIONES Y EVENTOS
+function load_page(){
+    alert("LA PAGINA SE HA CARGADO CORRECTAMENTE");
+}
+
+function change_color(){
+    document.body.style.backgroundColor = "red";
+    document.body.style.color = "#fff";
+}
+
+const btn_limpiar = document.querySelector("#clear_color");
+
+btn_limpiar.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+});
+
+//REGISTRO FORMULARIO
+const form_register = document.getElementById("form_register");
+const nombres = document.getElementById("nombres");
+const apellidos = document.getElementById("apellidos");
+const information = document.getElementById("information");
+
+form_register.addEventListener("submit", name_event =>{
+    name_event.preventDefault();
+    let info ="";
+    console.log(nombres.value.length);
+    // console.log(nombres.value);
+    if(nombres.value.length <= 2 || apellidos.value.length <=2){
+        info += "NOMBRES Y/O APELLIDOS INVÁLIDOS";
+        information.style.color = "red";
+    }
+    else{
+        alert(nombres.value + " " + apellidos.value);
+    }
+    information.innerText = info;
+})
