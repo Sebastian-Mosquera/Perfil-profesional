59296124636f9656488b03d42c328405fdc7a52f
Sebastian-Mosquera <jsmosquerab@ufpso.edu.co>
2023-11-10 14:18:57 -0500

    learning functions

--- js/functions.js
+++ js/functions.js
@@ -1,135 +1,139 @@
-// alert("FUNCTIONS ACTIVO");
-// ############# tipo de variables
-// const para valores constantes
-const pi = 3.1416;
+// alert("function activo");
+//#################### tipo de varibles//
+// const (valores constantes)
+const pi = 3.1415;
 const pul = 2.54;
-// var para variables globales 
+// var (para variables globales)
 var days = 10;
-var name = "Sebasian Mosquera";
-// let para variables de bloque o locales
+var name = "Sebastián Mosquera";
+// let (variables de bloque o locales)
 let count = 1;
 
-// ############### formas de impresion 
+//################# formas de impresion//
 // alert
 // alert(name);
 // console.log
 console.log(name);
-// body - pantalla 
-document.write(name + "Sebastian Mosquera");
-document.getElementById("box_one").innerHTML = "<h1>Sebastian Mosquera</h1>"
-document.getElementById("box_two").innerText = "<h1>Sebastian Mosquera</h1>"
+// body - pantalla
+document.write(name + "###Sebastián Mosquera")
+document.getElementById("box_one").innerHTML= "<h1>Sebastián Mosquera</h1>";
+document.getElementById("box_two").innerText= "<h1>Sebastián Mosquera</h1>";
 // librerias
-// Swal.fire(
-//     'The Internet?',
-//     'That thing is still around?',
-//     'question'
-//   )
+Swal.fire(
+    'The Internet?',
+    'That thing is still around?',
+    'question'
+  )
 Swal.fire({
     icon: 'error',
-    title: name,
+    ShowConfirmButton: false,
+    title:name,
     text: name,
     footer: "footer: " + name,
-    background:'green',
-    position: 'top-start'
+    background: 'white',
+    position:'top-start' 
 });
 
-
-// ########## tipos de datos
-// numericos
-var number_one = 20;
-var number_two = 5.5;
+//##################tipos de datos
+//numericos
+var number_one=20;
+var number_two=5.5;
 // texto
-var class_type = "Eléctiva Técnica 1";
-// booleanos 
-var boolean = true; //false
-
-// arrays - vectores 
-var array_num = [1,2,3,45,3.4,1.2];
-var array_text = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado", "Domingo"];
-var array_mix = [1, "a",2,"c"];
-var array_mul = [
-    {name:"Sebastián", last_name:"Mosquera",age:"20"},
-    {name:"Andres", last_name:"Rojas",age:"19"},
-    {name:"Julian", last_name:"Enrique",age:"18"},
-    {name:"Pedro", last_name:"Gonzales",age:"22"}
-
+var class_type= "Electiva Tecnica 1";
+// booleanos
+ var boolean = true; //false
+//  arrays - vectores
+var array_num=[1,2,3,45,3.4,1.2];
+var array_text=["lunes", "Martes", "Miercoles", "jueves", "Viernes", "Sabado", "Domingo"];
+var array_mix=[1, "a",2, "c"]
+var array_mul=[
+    {name:"Sebastian", last_name:"Mosquera", age:"20"},
+    {name:"Juan", last_name:"Gutierrez", age:"15"},
+    {name:"Pedro", last_name:"Ramirez", age:"43"},
+    {name:"Hector", last_name:"Hernandez", age:"73"}
 ]
 
-
-// ########### operadores básicos
+//#################### operadores basicos
 // suma
-var suma = number_one + number_two;
-console.log("Suma"  + suma);
+var suma= number_one + number_two;
+console.log("Suma = " +suma);
 // resta
-var resta = number_one - number_two;
-console.log("Resta"  + resta);
-// multiplicación
-var multi = number_one * number_two;
-console.log("Multiplicaión"  + multi);
-// division
-var divi = number_one / number_two;
-console.log("División"  + divi);
-// modulo
-var modulo = number_one % number_two;
-console.log("Módulo"  + modulo);
+var resta= number_one - number_two;
+console.log("Resta = " +resta);
+// multiplicacion 
+var multi= number_one * number_two;
+console.log("Multiplicacion = " +multi)
+// Division 
+var divi= number_one / number_two;
+console.log("Division = " +divi.toFixed(2));
+// Modulo 
+var modulo= number_one % number_two;
+console.log("Modulo = " +modulo);
+
+// OPERADORES LOGICOS (INVERSE,INCREMENTO Y DECREMENTO)
+// AND && con if (&&=ampersand)
+// "=" (asignacion), "==" (comparacion), "===" (compara el dato y el tipo de valor)
+var bool=true;
+var numeric= 10;
+if(bool && numeric==="10"){
+    console.log("Entra If");
+} 
+else{
+    console.log("Entra Else");
+}
+// OR || CON IF 
+var bool=true;
+var numeric= 7;
+if(bool || numeric ==7){
+    console.log("Entra If");
+    numeric+=3;
+}
+else{
+    console.log("Entra Else");
+    numeric--;}
+console.log(numeric);
+
+// FOR
+// console.log(("longitud: ") + array_text.length)
+for(let j =0; j<array_text.length; j++){
+    console.log(array_text[j] + " " + (j+1))
+}
+
+// While
+let s=0;
+console.log("While");
+while(s<array_text.length){
+    console.log(array_text[s] + " " + (s+1));
+    s++
+}
+//  DO WHILE
+let m = 0;
+console.log("Do While");
+do{
+    console.log(array_text[m] + " " + (m+1));
+    m++;
+}
+while(m<array_text.length)
 
 
-
-// OPERADORES LÓGICOS Y ESTRUCTURAS CONDICIONALES (INVERSE, INCREMNETO Y DEMCREMENTO)
-// AND && con if - Ampersand
-// var bool = false;
-// var numeric =10;
-// if(!bool && numeric == "10"){ // bool == true
-//     console.log("Entra if");
-// }
-// else{
-//     console.log("Entra else");
-// }
-// // OR || con if 
-// if(!bool || numeric == 7 ){
-//     console.log("Entra if");
-//     numeric+=3;
-// }else{
-//     console.log("Entra else");
-//     numeric--;
-// }
-
-// // FOR
-// for(let j = 0; j<array_text.length; j++){
-//     console.log(array_text[j] + (j+1));
-// }
-
-// // WHILE
-// let j = 0;
-// while(j<array_text.length){
-//     console.log(array_text[j] + (j+1));
-// }
-
-// // DO WHILE
-// let m = 0;
-// do{
-//     console.log(array_text[m] + (m+1));
-//     m++;
-// }while(m<array_text.length);
-
-//FUNCIONES Y EVENTOS
+// FUNCIONES Y EVENTOS
 function load_page(){
-    alert("LA PAGINA SE HA CARGADO CORRECTAMENTE");
+    // alert("LA PAGINA HA CARGADO CORRECTAMENTE")
 }
 
 function change_color(){
-    document.body.style.backgroundColor = "red";
-    document.body.style.color = "#fff";
+    document.body.style.backgroundColor ="red"
+    document.body.style.color="#fff"
 }
 
 const btn_limpiar = document.querySelector("#clear_color");
 
-btn_limpiar.addEventListener("click", () => {
-    document.body.style.backgroundColor = "#fff";
-    document.body.style.color = "#000";
+clear_color.addEventListener("click", () => {
+    document.body.style.backgroundColor ="#fff"
+    document.body.style.color="#000"
 });
 
-//REGISTRO FORMULARIO
+// REGISTRO FORMULARIO
 const form_register = document.getElementById("form_register");
 const nombres = document.getElementById("nombres");
 const apellidos = document.getElementById("apellidos");
@@ -137,15 +141,43 @@ const information = document.getElementById("information");
 
 form_register.addEventListener("submit", name_event =>{
     name_event.preventDefault();
-    let info ="";
-    console.log(nombres.value.length);
-    // console.log(nombres.value);
+    let info = "";
+    console.log(nombres.value);
     if(nombres.value.length <= 2 || apellidos.value.length <=2){
-        info += "NOMBRES Y/O APELLIDOS INVÁLIDOS";
-        information.style.color = "red";
+        info += "NOMBRES Y/O APELLIDOS INVALIDOS";
+        information.style.color= "red";
     }
     else{
-        alert(nombres.value + " " + apellidos.value);
+        alert(nombres.value + " " + apellidos.value)
     }
     information.innerText = info;
 })
+// FECHA Y EJERCICIO DE ARRAY
+function load_page(){
+    document.getElementById("nombres").focus();
+    document.getElementById("apellidos").disabled = true;
+    let date = new Date();
+    console.log(date.getMinutes());
+    for(let j=0; j<array_mul.length;j++){
+        console.log(array_mul[j].name);
+    }
+}
+
+function validate(){
+    let nombres = document.getElementById("nombres").value;
+    let nombres_style = document.getElementById("nombres");
+    if (nombres.length > 2){
+        nombres_style.border = "2px solid green";
+        document.getElementById("apellidos").disabled = false;
+        document.getElementById("apellidos").focus();
+        document.getElementById("nombres").value = "";
+        alert(nombres.concat(" Gutierrez "));
+    }
+}
+// let nombres = document.getElementById("nombres");
+//     console.log(nombres);
+//     
+//     }
+//     else{
+//         alert("Nombre invalido")
+//     }
